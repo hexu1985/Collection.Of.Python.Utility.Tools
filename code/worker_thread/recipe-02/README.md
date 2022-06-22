cd1a443a732bde33c5f9da11fadc9f4b5d4f53fc
hexu <hexu_1985@sina.com>
2022-06-22 16:30:58 +0800

    add worker_thread/recipe-02

---
+++ code/worker_thread/recipe-02/README.md
@@ -0,0 +1,1 @@
+工作线程类, 基于Command模式, 每个Task只要是callable对象就行
---
+++ code/worker_thread/recipe-02/test_worker_thread.py
@@ -0,0 +1,38 @@
+from worker_thread import WorkerThread
+import time
+import functools
+import logging
+
+def test_func():
+    raise TypeError("abcde")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    class DemoTask:
+        def __init__(self, name):
+            self.name = name
+
+        def print_int(self, i):
+            print("execute {} - {}".format(self.name, i))
+            time.sleep(1.0)
+
+    print("test of worker_thread.py")
+    worker = WorkerThread()
+    worker.start()
+
+    task = DemoTask("demo task") 
+    print("start task")
+    for i in range(1, 5):
+        worker.putTask(functools.partial(task.print_int, i))
+
+    worker.putTask(test_func)
+
+    for i in range(6, 10):
+        worker.putTask(functools.partial(task.print_int, i))
+
+    print("stop worker thread")
+    worker.stop()
+
+    print("join worker thread")
+    worker.join()
+    print("exit of worker_thread.py")
