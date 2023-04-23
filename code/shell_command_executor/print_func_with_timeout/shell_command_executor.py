b982df99b1403d40bb5323b0a5fdc4406b2cb025
hexu <hexu_1985@sina.com>
2023-04-23 11:19:27 +0800

    update print_func_with_timeout

--- code/shell_command_executor/print_func_with_timeout/shell_command_executor.py
+++ code/shell_command_executor/print_func_with_timeout/shell_command_executor.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 import subprocess
-import signal
-import time
-import os
 import logging
 import threading
+import time
+import os
+import psutil
+import signal
 
 LOGGER = logging.getLogger()
+
+class AllChildPidList:
+    def __init__(self, pid):
+        self.pid = pid
+        self.child_pid_list = []
+
+    def walk(self, pid):
+        p = psutil.Process(pid)
+        for child in p.children():
+            self.child_pid_list.append(child.pid)
+            self.walk(child.pid)
+
+    def get(self):
+        if not self.child_pid_list:
+            self.walk(self.pid)
+
+        return self.child_pid_list
 
 class ShellCommandExecutor:
     def __init__(self, cmd, timeout=3600, print_func=None):
@@ -16,13 +34,14 @@ class ShellCommandExecutor:
         self.timeout = timeout
         self.proc = None
         self.start_time = None
-        LOGGER.info("create ShellCommandExecutor(cmd=[{}])".format(self.cmd))
+        self.pipe_r, self.pipe_w = os.pipe()
+        LOGGER.debug("create ShellCommandExecutor(cmd=[{}])".format(self.cmd))
 
     def print_output(self, message):
         if self.print_func:
             self.print_func(message)
         else:
-            LOGGER.info("cmd: [{}] output: {}".format(self.cmd, message))
+            LOGGER.debug("cmd: [{}] output: {}".format(self.cmd, message))
 
     def __str__(self):
         return 'ShellCommandExecutor("{}")'.format(self.cmd)
@@ -32,10 +51,11 @@ class ShellCommandExecutor:
 
     def run(self):
         try:
-            self.proc = subprocess.Popen(self.cmd, shell=True, close_fds=True, preexec_fn=os.setpgrp,
-                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            self.proc = subprocess.Popen(self.cmd, shell=True, close_fds=True,
+                    stdout=self.pipe_w, stderr=self.pipe_w)
             self.start_time = time.time()
-            LOGGER.info('run cmd [{}]'.format(self.cmd))
+            LOGGER.debug('run cmd [{}]'.format(self.cmd))
+            os.close(self.pipe_w)
         except subprocess.CalledProcessError as err:
             LOGGER.error('run cmd [{}] error: {}'.format(self.cmd, err))
 
@@ -44,12 +64,13 @@ class ShellCommandExecutor:
             LOGGER.error("cmd: [{}] is not run".format(self.cmd))
             return -1
 
-        def print_output(cmd, output, print_func):
+        def print_output(cmd, pipe_r, print_func):
+            output = os.fdopen(pipe_r)
             for line in output:
-                print_func(line.decode().rstrip())
-            LOGGER.info("cmd: [{}] output thread exit".format(cmd))
+                print_func(line.rstrip())
+            LOGGER.debug("cmd: [{}] output thread exit".format(cmd))
 
-        t = threading.Thread(target=print_output, args=(self.cmd, self.proc.stdout, self.print_output), daemon=True)
+        t = threading.Thread(target=print_output, args=(self.cmd, self.pipe_r, self.print_output), daemon=True)
         t.start()
 
         try:
@@ -59,8 +80,11 @@ class ShellCommandExecutor:
             LOGGER.info("cmd: [{}] complete with ret: {}, escaped time: {}s".format(self.cmd, ret, int(now-self.start_time)))
             return ret
         except subprocess.TimeoutExpired as err:
-            os.killpg(self.proc.pid, signal.SIGKILL)
-            LOGGER.info("cmd: [{}] was killed because timeout".format(self.cmd))
+            all_child_pid_list = AllChildPidList(self.proc.pid)
+            for pid in all_child_pid_list.get():
+                os.kill(pid, signal.SIGKILL)
+            self.proc.kill()
+            LOGGER.error("cmd: [{}] was killed because timeout".format(self.cmd))
             return -1
 
 
