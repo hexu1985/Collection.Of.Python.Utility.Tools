32ce622f510dab389f31f1b24a77ce97d26ec532
hexu <hexu_1985@sina.com>
2024-07-04 15:24:44 +0800

    update ply

---
+++ ply/examples/lex/recipe-01/example2.py
@@ -0,0 +1,14 @@
+from calclex import lexer
+
+# Test it out
+data = '''
+3 + 4 * 10
+  + -20 *2
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+for tok in lexer:
+    print(tok)
---
+++ ply/examples/lex/recipe-01/example3.py
@@ -0,0 +1,16 @@
+from calclex import lexer
+
+# Test it out
+data = '''
+3 + 4 * 10
+  + -20 *2
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: break      # No more input
+    print(tok.type, tok.value, tok.lineno, tok.lexpos)
