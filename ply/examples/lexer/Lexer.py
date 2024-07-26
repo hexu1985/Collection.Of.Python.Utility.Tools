9031793ebca08861469be74cb61752bb7c544fb4
hexu <hexu_1985@sina.com>
2024-07-26 11:06:32 +0800

    add ply/examples/lexer

---
+++ ply/examples/lexer/Lexer.py
@@ -0,0 +1,59 @@
+import ply.lex as lex
+
+class Lexer:
+    # List of token names.   This is always required
+    tokens = (
+            'NUMBER',
+            'IDENTIFIER',
+            'STRING',
+            'COMMENT',
+            )
+
+    # A regular expression rule with some action code
+    # Note addition of self parameter since we're in a class
+    def t_NUMBER(self, t):
+        r'[0-9]+'
+        t.value = int(t.value)    
+        return t
+
+    def t_IDENTIFIER(self, t):
+        r'[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||[!"#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~]'
+        return t
+
+    def t_STRING(self, t):
+        r'"(\\"|\\\\|\\n|[^"])*"'
+        return t
+
+    # Define a rule so we can track line numbers
+    def t_newline(self,t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    t_COMMENT = r'//.*'
+
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore = ' \t'
+
+    # Error handling rule
+    def t_error(self,t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    # Build the lexer
+    def build(self,**kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+    
+    # Test it output
+    def test(self,data):
+        self.lexer.input(data)
+        while True:
+             tok = self.lexer.token()
+             if not tok: break
+             print("==>", tok.value)
+
+if __name__ == "__main__":
+    # Build the lexer and try it out
+    m = Lexer()
+    m.build()           # Build the lexer
+    with open('test.txt') as f:
+        m.test(f.read())     # Test it
